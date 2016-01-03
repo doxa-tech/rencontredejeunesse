9e54aaaa8644ebdc3a489231a5caf948245645cf
khcr <kocher.ke@gmail.com>
2016-01-03 21:17:53 +0100

    content

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -1,6 +1,12 @@
 document.body.onclick = function(e) {
-    var target = e.target;
-    if(target.classList.contains("lightbox")) {
-
-    }
+  var target = e.target;
+  if(target.classList.contains("lightbox-open")) {
+    target.nextElementSibling.style.display = "flex";
+  }
+  if(target.classList.contains("lightbox-close")) {
+    target.parentElement.parentElement.parentElement.style.display = "none";
+  }
+  if(target.classList.contains("lightbox-wrap")) {
+    target.style.display = "none";
+  }
 };
