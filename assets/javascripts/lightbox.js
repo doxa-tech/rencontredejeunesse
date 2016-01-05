bb95134202cce7fc767d23d926583ce63588bfbd
khcr <kocher.ke@gmail.com>
2016-01-05 16:47:18 +0100

    no scroll with lightbox

--- assets/javascripts/lightbox.js
+++ assets/javascripts/lightbox.js
@@ -3,13 +3,16 @@ document.body.onclick = function(e) {
   if(target.classList.contains("lightbox-open")) {
     var $content = document.getElementById(target.getAttribute("content"));
     fadeIn($content, function() {
+      document.body.classList.add("noscroll");
       $content.style.display = "flex";
     });
   }
   if(target.classList.contains("lightbox-close")) {
+    document.body.classList.remove("noscroll");
     target.parentElement.parentElement.parentElement.style.display = "none";
   }
   if(target.classList.contains("lightbox-wrap")) {
+    document.body.classList.remove("noscroll");
     target.style.display = "none";
   }
 };
