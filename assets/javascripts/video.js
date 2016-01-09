16f63d1088aa23fa2e8aa54573505839969f65c2
khcr <kocher.ke@gmail.com>
2016-01-09 16:16:05 +0100

    javascript for youtube video

--- assets/javascripts/video.js
+++ assets/javascripts/video.js
@@ -1,20 +1,20 @@
-var $video = document.getElementById("home-video"),
-    isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+var $poster = document.getElementById("poster");
 
-if(isFirefox) { // Firefox
-  $video.addEventListener('click', function () {
-    if (!this.currentTime) {
-  	  this.controls = true;
-      this.play();
-    }
-  });
-} else { // Other browsers
-  $video.addEventListener('click', function () {
-    if (this.paused) {
-  	  this.controls = true;
-      this.play();
-    } else {
-      this.pause();
-    }
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('home-video', {
+    height: '100%',
+    width: '100%',
+    videoId: '_Scym_ZG2Vw',
   });
 }
+
+$poster.addEventListener("click", function() {
+  $poster.style.visibility = "hidden";
+  player.playVideo();
+});
