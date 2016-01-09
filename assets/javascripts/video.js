2b6dc6f9ea775186da5fdab556915d0dd56e8247
khcr <kocher.ke@gmail.com>
2016-01-09 16:58:39 +0100

    fix youtube player

--- assets/javascripts/video.js
+++ assets/javascripts/video.js
@@ -8,11 +8,24 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('home-video', {
+    width: '100%',
+    height: '100%',
     videoId: '_Scym_ZG2Vw',
+    events: {
+      'onReady': onPlayerReady
+    }
   });
+}
+
+function onPlayerReady() {
+  if($poster.style.visibility === "hidden") {
+    player.playVideo();
+  }
 }
 
 $poster.addEventListener("click", function() {
   $poster.style.visibility = "hidden";
-  player.playVideo();
+  if(player) {
+    player.playVideo();
+  }
 });
