a80b3a9edb71736c1176cb711634ffce9b2b04f8
khcr <kocher.ke@gmail.com>
2016-10-30 08:45:31 +0100

    login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
 
   def index
   end
-  
+
+  def login
+    render layout: "login"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: "pages#index"
 
+  get "login", to: "pages#login"
+
   #
   # ORDERS
   #
