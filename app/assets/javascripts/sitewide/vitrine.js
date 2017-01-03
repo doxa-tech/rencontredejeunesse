649f98ed1a2a360a725dd347e90d25551b6fd99b
Noémien Kocher <nkcr.je@gmail.com>
2017-01-03 13:43:59 +0100

    mobile compatibility + extra animation

--- app/assets/javascripts/sitewide/vitrine.js
+++ app/assets/javascripts/sitewide/vitrine.js
@@ -1,10 +1,15 @@
 $(document).on("ready page:load", function() {
   var soon = $("#soon");
   var behind = $("#behind");
+  var pulse = $("#pulse");
+
   soon.click(function() {
     soon.addClass("open");
+    pulse.hide();
   });
   behind.click(function() {
     soon.removeClass("open");
+    pulse.show();
   });
+
 });
