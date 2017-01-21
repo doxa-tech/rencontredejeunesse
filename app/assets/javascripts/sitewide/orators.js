a940baa79b45aba57fcf12f7ef64cb253631cd9d
khcr <kocher.ke@gmail.com>
2017-01-22 00:06:58 +0100

    orators

--- app/assets/javascripts/sitewide/orators.js
+++ app/assets/javascripts/sitewide/orators.js
@@ -1,11 +1,16 @@
 $(document).on("turbolinks:load", function() {
 
   $(".person").click(function() {
-    $(".text.active").css("height", 0).removeClass("active");
-    $(this).find(".text").animate({
-      height: "270px"
-    }, 800, function() {
-      $(this).addClass("active");
-    });
+    var $text = $(this).find(".text");
+    if($text.hasClass("active")) {
+      $text.css("height", 0).removeClass("active");
+    } else {
+      $(".text.active").css("height", 0).removeClass("active");
+      $text.animate({
+        height: "270px"
+      }, 800, function() {
+        $text.addClass("active");
+      });
+    }
   });
 });
