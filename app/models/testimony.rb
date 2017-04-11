274d2332cddae8d28d9f04cef62394c75b1bdf1b
Noémien Kocher <nkcr.je@gmail.com>
2017-04-11 22:05:30 +0200

    testimony, strip content

--- app/models/testimony.rb
+++ app/models/testimony.rb
@@ -3,6 +3,12 @@ class Testimony < ApplicationRecord
 
   belongs_to :user
 
+  before_validation :strip_content
+
   validates :message, presence: true, length: { maximum: 500 }
   validates :user, presence: true
+
+  def strip_content
+    self.message = self.message.strip unless self.message.nil?
+  end
 end
