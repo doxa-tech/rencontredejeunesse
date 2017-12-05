e9e4de1b7275c10f71980488044c459953d5756d
Keran Kocher <kocher.ke@gmail.com>
2017-12-05 20:44:38 +0100

    navigation & history

---
+++ app/assets/javascripts/sitewide/history.js
@@ -0,0 +1,21 @@
+$(document).on("turbolinks:load", function() {
+
+  var $historyLinks = $("#history").find(".history-links");
+
+  $historyLinks.on("click", "p", function(e) {
+    var $currentLink = $historyLinks.find(".selected");
+    var currentHistory = $currentLink.attr("data-content");
+    var $currentHistory = $("#" + currentHistory);
+
+    var $newLink = $(this);
+    var newHistory = $newLink.attr("data-content");
+    var $newHistory = $("#" + newHistory);
+
+    $currentLink.removeClass("selected");
+    $newLink.addClass("selected");
+    $currentHistory.removeClass("selected");
+    $newHistory.addClass("selected");
+
+  });
+
+});
