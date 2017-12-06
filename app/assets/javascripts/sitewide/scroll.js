7f9d560444e5850730f0ea34fc9efcd756ab72af
Keran Kocher <kocher.ke@gmail.com>
2017-12-06 20:08:28 +0100

    fix nav

--- app/assets/javascripts/sitewide/scroll.js
+++ app/assets/javascripts/sitewide/scroll.js
@@ -1,5 +1,21 @@
 $(document).on("turbolinks:load", function() {
 
-  gumshoe.init();
+  var closeBtn = document.getElementById("close-header");
+  var headerBtn = document.getElementById("header-btn");
+  var headerFrame = document.getElementById("header-frame");
+
+  var hideHeader = function() {
+    headerFrame.style.display = "none";
+  }
+
+  headerBtn.onclick = function() {
+    headerFrame.style.display = "block";
+  }
+
+  closeBtn.onclick = hideHeader;
+
+  gumshoe.init({
+    callback: hideHeader
+  });
 
 });
