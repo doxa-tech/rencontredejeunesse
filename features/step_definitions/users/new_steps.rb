a1b3b81e5576e37304ad16b862bfe026e2e6f620
Keran Kocher <kocher.ke@gmail.com>
2018-01-06 12:29:00 +0100

    fix tests

--- features/step_definitions/users/new_steps.rb
+++ features/step_definitions/users/new_steps.rb
@@ -4,7 +4,7 @@ When("I successfully complete the signup form") do
     fill_in "Prénom", with: "John"
     fill_in "Nom de famille", with: "Smith"
     fill_in "Email", with: "john@smith.com"
-    fill_in "Téléphone", with: "1234567890"
+    fill_in "Téléphone", with: "+41790000000"
     fill_in "Adresse", with: "Route de la fruitière 18"
     fill_in "NPA", with: "1291"
     fill_in "Ville", with: "Le Moulin"
