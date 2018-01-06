fa60811c1ca61a539a014ec1b3e983db5b281b6a
Noémien Kocher <nkcr.je@gmail.com>
2018-01-06 15:20:15 +0100

    order design

--- app/helpers/standard_form_builder.rb
+++ app/helpers/standard_form_builder.rb
@@ -3,13 +3,13 @@ class StandardFormBuilder < ActionView::Helpers::FormBuilder
     def field_with_errors(type, attribute, options = {})
       field = self.send("#{type}_field", attribute, options)
       message = error_message_for(attribute)
-      html_wrapper(field, message)
+      html_wrapper(message, field)
     end
 
     def checkbox_with_errors(attribute, options = {})
       field = self.check_box(attribute, options)
       message = error_message_for(attribute)
-      html_wrapper(field, message)
+      html_wrapper(message, field)
     end
 
     private
@@ -18,8 +18,8 @@ class StandardFormBuilder < ActionView::Helpers::FormBuilder
       @object.errors.full_messages_for(attribute).join(" / ")
     end
 
-    def html_wrapper(field, message)
-      %(<div class="field">#{field}</div><div class="error-message">#{message}</div>).html_safe
+    def html_wrapper(message, field)
+      %(<div><div class="error-message">#{message}</div><div class="field">#{field}</div></div>).html_safe
     end
 
 end
