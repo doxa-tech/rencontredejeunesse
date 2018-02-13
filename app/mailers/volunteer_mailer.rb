1109cf129d2c07b235674e1469b180c49d19b7d5
Keran Kocher <kocher.ke@gmail.com>
2018-02-13 17:13:53 +0100

    email to announce order for volunteer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -6,4 +6,9 @@ class VolunteerMailer < ApplicationMailer
     mail(to: volunteer.user.email, subject: "Merci pour votre engagement !")
   end
 
+  def order_opened
+    emails = Volunteer.includes(:user).map { |v| v.user.email }
+    mail(to: "Bénévoles <noreply@rencontredejeunesse.ch>", bcc: emails, subject: "Commande ton entrée comme bénévole !")
+  end
+
 end
