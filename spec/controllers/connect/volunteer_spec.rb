640f7e0a5224d423458a67337d6bfd361166d6d1
Keran Kocher <kocher.ke@gmail.com>
2018-04-23 00:18:00 +0200

    tests for volunteer order

--- spec/controllers/connect/volunteer_spec.rb
+++ spec/controllers/connect/volunteer_spec.rb
@@ -1,13 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Connect::VolunteersController, :type => :controller do
+  before(:each) do
+    @user = create(:user)
+    cookies[:remember_token] = { value: @user.remember_token }
+  end
+
+  it "saves the right amount without lodging" do
+    post :confirmation
+    expect(assigns(:order).amount).to eq 5300
+  end
+
+  it "saves the right amount with lodging" do
+    post :confirmation, params: { lodging: "1" }
+    expect(assigns(:order).amount).to eq 8300
+  end
+
+  it "applies a discount" do
+    discount = create(:discount, category: :free, number: 1)
+    post :confirmation, params: { discount_code: discount.code }
+    expect(assigns(:order).amount).to eq 0
+  end
 
   it "saves the amount of the discount" do
-    user = create(:user)
     discount = create(:discount, category: :free, number: 1)
-    cookies[:remember_token] = { value: user.remember_token }
     post :confirmation, params: { discount_code: discount.code }
-    expect(assigns(:order).discount_amount).to eq 53
+    expect(assigns(:order).discount_amount).to eq 5300
   end
 
 end
