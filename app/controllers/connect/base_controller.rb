d174a1afd03245a426515b4531e1b3c96b1e651b
Keran Kocher <kocher.ke@gmail.com>
2018-04-26 11:16:16 +0200

    sign_in_with_token

--- app/controllers/connect/base_controller.rb
+++ app/controllers/connect/base_controller.rb
@@ -1,6 +1,5 @@
 class Connect::BaseController < ApplicationController
-  before_action :check_if_signed_in # defined in ApplicationController
-  before_action :check_if_confirmed
+  before_action :sign_in_with_token, :check_if_signed_in, :check_if_confirmed
 
   layout "connect"
 
@@ -9,5 +8,11 @@ class Connect::BaseController < ApplicationController
   def check_if_confirmed
     # TODO: different formats supported
     render "connect/unconfirmed" unless current_user.confirmed
+  end
+
+  def sign_in_with_token
+    if params[:remember_token] && user = User.find_by_remember_token(params[:remember_token])
+      sign_in(user)
+    end
   end
 end
