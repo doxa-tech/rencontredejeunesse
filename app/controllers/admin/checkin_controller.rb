926b478c344d6fe4b17ef27b3279f7890d69ffc9
Noémien Kocher <nkcr.je@gmail.com>
2018-05-03 18:04:17 +0200

    checkin design

--- app/controllers/admin/checkin_controller.rb
+++ app/controllers/admin/checkin_controller.rb
@@ -9,16 +9,23 @@ class Admin::CheckinController < Admin::BaseController
   def show
     authorize!
     @order = Order.find_by!(order_id: params[:id])
+    @state = "ok"
+    if !@order.note.blank?
+      @state = "infos"
+    elsif @order.status != 9 || @order.delivered
+      @state = "nok"
+    end
   end
 
   # Look for the order
   def create
     authorize!
+    p params[:order_id]
     @order = Order.find_by(order_id: params[:order_id])
     unless @order.nil?
       redirect_to admin_checkin_path(params[:order_id])
     else
-      flash.now[:error] = "Commande non trouvé !"
+      flash.now[:error] = "Commande non trouvée !"
       render 'index'
     end
   end
