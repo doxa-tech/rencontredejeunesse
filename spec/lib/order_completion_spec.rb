fc82588c5ed9fbfe366fcbd49ccf1c616b8ff011
Keran Kocher <kocher.ke@gmail.com>
2018-06-26 15:23:56 +0200

    order_completion_spec.rb passed

--- spec/lib/order_completion_spec.rb
+++ spec/lib/order_completion_spec.rb
@@ -1,62 +1,72 @@
 require 'rails_helper'
+require "#{Rails.root}/lib/order_completion.rb"
 
 RSpec.describe OrderCompletion do
 
   it "rejects an order with the payment method postfinance" do
-    order = create(:order)
-    post :complete, params: { id: order.order_id }
-    order.reload
-    expect(response).to redirect_to "/orders/#{order.product_name}/#{order.order_id}/confirmation"
+    order = create(:event_order)
+    completion = OrderCompletion.new(order)
+    expect { completion.complete }.to raise_error ArgumentError
   end
 
   describe "#common_steps" do
   
     it "sets a discount as used" do
-      @order.discount = create(:discount)
-      @order.save!
-      post :update, params: {
-        orderID: @order.order_id, amount: @order.amount, STATUS: 5, PAYID: 3010824561, NCERROR: 0, SHASIGN: shaout.upcase
-      }
-      @order.reload
-      expect(@order.discount.used).to eq true
+      order = create(:event_order, discount: create(:discount))
+      order.save!
+      OrderCompletion.new(order).complete(:postfinance)
+      expect(order.discount.used).to eq true
     end
 
     it "sends a confirmation email" do
-      expect {
-        post :update, params: {
-          orderID: @order.order_id, amount: @order.amount, STATUS: 5, PAYID: 3010824561, NCERROR: 0, SHASIGN: shaout.upcase
-        }
-      }.to change(ActionMailer::Base.deliveries, :size).by(2)
+      order = create(:event_order)
+      OrderCompletion.new(order).complete(:postfinance)
+      expect(ActionMailer::Base.deliveries.any? { |mail| mail.subject == "Confirmation de commande" }).to be true
     end
 
   end
 
   describe "#invoice" do
 
-    it "updates an order with the payment method invoice" do
-      participants = build_list(:rj_participant, 15, lodging: true)
-      product = create(:rj, participants: participants)
-      order = create(:order, product: product)
-      post :complete, params: { id: order.order_id }
-      order.reload
-      expect(order.status).to be 41
+    it "sets the payment status to 41" do
+      order = create(:event_order)
+      item = create(:item, price: 10000)
+      order.registrants = create_list(:registrant, 10, order: order, item: item)
+      order.save!
+      OrderCompletion.new(order).complete
+      expect(order.main_payment.status).to be 41
     end
 
   end
 
   describe "#postfinance" do
 
+    it "sends an email with the pass" do
+      order = create(:event_order)
+      OrderCompletion.new(order).complete(:postfinance)
+      expect(ActionMailer::Base.deliveries.any? { |mail| mail.subject == "Ticket pass pour ta commande" }).to be true
+    end
+
   end
 
   describe "#free" do
 
-    it "updates an order with an amount of zero" do
-      order = create(:order)
-      order.lump_sum = 0
-      order.save
-      post :complete, params: { id: order.order_id }
-      order.reload
-      expect(order.status).to be 9
+    it "sends an email with the pass" do
+      order = create(:event_order)
+      discount = create(:discount, category: :free, number: 1, items: order.items)
+      order.discount = discount
+      order.save!
+      OrderCompletion.new(order).complete
+      expect(ActionMailer::Base.deliveries.any? { |mail| mail.subject == "Ticket pass pour ta commande" }).to be true
+    end
+
+    it "sets the payment status to 41" do
+      order = create(:event_order)
+      discount = create(:discount, category: :free, number: 1, items: order.items)
+      order.discount = discount
+      order.save!
+      OrderCompletion.new(order).complete
+      expect(order.main_payment.status).to be 9
     end
 
   end
