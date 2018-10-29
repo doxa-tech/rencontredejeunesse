a31b924501705d620b2b4afb4387b2a6e7d332d4
Keran Kocher <kocher.ke@gmail.com>
2018-10-29 21:58:42 +0100

    remove static attributes from factories.rb

--- Gemfile
+++ Gemfile
@@ -25,11 +25,6 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
-# Use ActiveModel has_secure_password
-# gem 'bcrypt', '~> 3.1.7'
-
-# Use Capistrano for deployment
-# gem 'capistrano-rails', group: :development
 
 # HTML select list for countries
 gem 'country_select'
@@ -96,9 +91,6 @@ gem 'mime-types'
 
 # Pagination
 gem 'will_paginate', '~> 3.1.0'
-
-# Code coverage
-gem 'codeclimate-test-reporter', group: :test, require: nil
 
 # PDF generation
 gem 'prawn'
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,19 +1,19 @@
 FactoryBot.define do
 
   factory :user do
-    firstname "John"
-    lastname "Smith"
+    firstname { "John" }
+    lastname { "Smith" }
     email { "#{firstname.downcase}@#{lastname.downcase}.com" }
-    address "Route du chemin 1"
+    address { "Route du chemin 1" }
     npa 1630
-    city "Bulle"
-    phone "+41790000000"
-    country "CH"
+    city { "Bulle" }
+    phone { "+41790000000" }
+    country { "CH" }
     birthday { Date.new(1996, 2, 15) }
-    gender "male"
-    password "carottes"
-    password_confirmation "carottes"
-    confirmed true
+    gender { "male" }
+    password { "carottes" }
+    password_confirmation { "carottes" }
+    confirmed { true }
 
     factory :volunteer do
       after(:create) do |user|
@@ -24,44 +24,44 @@ FactoryBot.define do
   end
 
   factory :item do
-    name "Rencontre de jeunesse 2018 - WE"
-    description "RJ 2018"
-    price 6000
-    active true
+    name { "Rencontre de jeunesse 2018 - WE" }
+    description { "RJ 2018" }
+    price { 6000 }
+    active { true }
     number { Random.new.rand(1000..9999) }
-    key "rj"
+    key { "rj" }
   end
 
   factory :discount do
-    category "money"
-    reduction 2000
+    category { "money" }
+    reduction { 2000 }
   end
 
   factory :registrant do
-    gender "male"
-    firstname "Patrick"
-    lastname "Johnson"
-    birthday Date.new(1996, 02, 15)
+    gender { "male" }
+    firstname { "Patrick" }
+    lastname { "Johnson" }
+    birthday { Date.new(1996, 02, 15) }
     item
     association :order, factory: :event_order
   end
 
   factory :order_item do
-    quantity 1 
+    quantity { 1 }
     item
     order
   end
 
   factory :order do
 
-    order_type "regular"
+    order_type { "regular" }
     user
-    pending false
+    pending { false }
     
     factory :order_with_items do
       transient do
-        number 1
-        quantity 1
+        number { 1 }
+        quantity { 1 }
       end
 
       after(:create) do |order, evaluator|
@@ -72,10 +72,10 @@ FactoryBot.define do
 
     factory :event_order, class: "Orders::Event" do
       transient do
-        number 1
+        number { 1 }
       end
 
-      order_type "event"
+      order_type { "event" }
       registrants { build_list(:registrant, number, order: nil) }
     end
 
