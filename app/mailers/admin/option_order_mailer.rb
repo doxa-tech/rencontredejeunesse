b6e75bfb4342243f0e085fd60475c4af19e53336
Keran Kocher <kocher.ke@gmail.com>
2019-02-02 11:49:45 +0100

    update adeia to use token

--- app/mailers/admin/option_order_mailer.rb
+++
@@ -1,7 +0,0 @@
-class Admin::OptionOrderMailer < ApplicationMailer
-
-  def registration()
-    mail(to: ["daniel.darcey@gmail.com", "raphael.guichoud@gmail.com", "kocher.ke@gmail.com"], subject: "Nouveau bénévole confirmé")
-  end
-
-end
--- app/mailers/previews/admin/option_order_preview.rb
+++
@@ -1,7 +0,0 @@
-class Admin::OptionOrderPreview < ActionMailer::Preview
-
-  def registration
-    Admin::OptionOrderMailer.registration()
-  end
-
-end
