f0714555d54afbd79c91c63ee7218161a0d0d9f1
Keran Kocher <kocher.ke@gmail.com>
2019-02-12 11:41:00 +0100

    fix scoping in connect/orders

--- app/controllers/connect/orders_controller.rb
+++ app/controllers/connect/orders_controller.rb
@@ -1,5 +1,5 @@
 class Connect::OrdersController < Connect::BaseController
-  before_action :check_if_paid, only: [:show, :ticket]
+  before_action :check_if_paid, only: [:ticket]
 
   def index
     @orders = current_user.completed_orders
@@ -10,17 +10,14 @@ class Connect::OrdersController < Connect::BaseController
   end
 
   def show
-    respond_to do |format|
-      format.html
-    end
+    @order = order
   end
 
   def invoice
     respond_to do |format|
-      @order = Orders::Event.find_by_order_id!(params[:id])
       format.pdf do
-        pdf = InvoicePdf.new(@order.invoice_pdf_adapter)
-        send_data pdf.render, filename: "Facture_#{@order.order_id}.pdf", type: "application/pdf", disposition: 'inline'
+        pdf = InvoicePdf.new(order.invoice_pdf_adapter)
+        send_data pdf.render, filename: "Facture_#{order.order_id}.pdf", type: "application/pdf", disposition: 'inline'
       end
     end
   end
@@ -28,18 +25,21 @@ class Connect::OrdersController < Connect::BaseController
   def ticket
     respond_to do |format|
       format.pdf do
-        pdf = TicketPdf.new(@order.ticket_pdf_adapter)
-        send_data pdf.render, filename: "Tickets_#{@order.order_id}.pdf", type: "application/pdf", disposition: 'inline'
+        pdf = TicketPdf.new(order.ticket_pdf_adapter)
+        send_data pdf.render, filename: "Tickets_#{order.order_id}.pdf", type: "application/pdf", disposition: 'inline'
       end
     end
   end
 
   private
 
+  def order
+    @order ||= Orders::Event.where(order_id: params[:id], user: current_user).first!
+  end
+
   def check_if_paid
-    @order = Orders::Event.find_by_order_id!(params[:id])
-    if request.format.pdf? && !@order.paid?
-      redirect_to connect_order_path(@order.order_id)
+    unless order.paid?
+      redirect_to connect_order_path(order.order_id)
     end
   end
 end
