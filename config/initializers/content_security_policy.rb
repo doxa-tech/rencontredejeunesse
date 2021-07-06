6a3122c73b665e8c99d63448f484fd6af36efee7
Keran Kocher <kocher.ke@gmail.com>
2021-07-06 21:00:56 +0200

    Add a CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -4,17 +4,19 @@
 # For further information see the following documentation
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
-# Rails.application.config.content_security_policy do |policy|
-#   policy.default_src :self, :https
+Rails.application.config.content_security_policy do |policy|
+    policy.default_src :self, :https
 #   policy.font_src    :self, :https, :data
 #   policy.img_src     :self, :https, :data
 #   policy.object_src  :none
-#   policy.script_src  :self, :https
-#   policy.style_src   :self, :https
+    policy.script_src  :self, "https://hcaptcha.com", "https://*.hcaptcha.com", "https://maps.googleapis.com"
+    policy.style_src   :self, :unsafe_inline, "https://hcaptcha.com", "https://*.hcaptcha.com"
+    policy.frame_src   :self, "https://hcaptcha.com", "https://*.hcaptcha.com", "https://www.youtube.com", "https://www.youtube-nocookie.com"
+    policy.connect_src :self, "https://hcaptcha.com", "https://*.hcaptcha.com"
 
 #   # Specify URI for violation reports
 #   # policy.report_uri "/csp-violation-report-endpoint"
-# end
+end
 
 # If you are using UJS then enable automatic nonce generation
 # Rails.application.config.content_security_policy_nonce_generator = -> request { SecureRandom.base64(16) }
